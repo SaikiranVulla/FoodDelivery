4366fe868fbbd1d0102858aa248c8264c0a49de7
vulla sai kiran kumar <vullasaikiran@gmail.com>
2025-01-29 23:09:51 +0530

    Created a common Component

--- src/Components/BottomModal.js
+++ src/Components/BottomModal.js
@@ -1,14 +1,37 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import appTheme from '../../constants/theme';
 
-const BottomModal = () => {
+const BottomModal = ({children}) => {
   return (
-    <View>
-      <Text>BottomModal</Text>
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheet
+        snapPoints={['80%', '100%']}
+        backgroundStyle={styles.modalView}>
+        <BottomSheetView style={styles.subContainer}>
+          {children}
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
 export default BottomModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: appTheme.COLORS.logBackGround,
+  },
+  subContainer: {
+    flex: 1,
+    padding: 12,
+  },
+  modalView: {
+    borderTopRightRadius: 35,
+    borderTopLeftRadius: 35,
+    backgroundColor: appTheme.COLORS.lightGray2,
+  },
+});
--- src/Container/LoginScreen.js
+++ src/Container/LoginScreen.js
@@ -1,14 +1,53 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import React from 'react';
+import appTheme from '../../constants/theme';
+import BottomModal from '../Components/BottomModal';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor={appTheme.COLORS.logBackGround}
+        barStyle={'light-content'}
+        animated={true}
+      />
+      <View style={styles.subContainer}>
+        <Text style={styles.mainTitle}>Log In</Text>
+        <Text style={styles.subTitle}>
+          Please sign in to your existing account
+        </Text>
+      </View>
+      <View style={styles.modalContainer}>
+        <BottomModal>
+          <Text>Modal Content</Text>
+        </BottomModal>
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: appTheme.COLORS.logBackGround,
+  },
+  subContainer: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainTitle: {
+    color: appTheme.COLORS.white,
+    ...appTheme.FONTS.h1,
+  },
+  subTitle: {
+    color: appTheme.COLORS.white,
+    marginTop: 5,
+    ...appTheme.FONTS.h3,
+  },
+  modalContainer: {
+    flex: 0.7,
+  },
+});
